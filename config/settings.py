451978c867a9161a264489aad925cea3b9e1d4c3
Aminjon <aminrustamov0424@gmail.com>
2026-04-06 23:22:07 +0500

    make it accessable through any device in a local network

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--m8jfb#&o_whv977ukfs9l0wo+lse5gy5ewnrgde!dsx-oi)(8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
